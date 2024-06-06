853f0e2e4f781f012c4d51bd091d2bc0d3b2380c
KalandarovST <kalandarovtillo78@gmail.com>
2024-06-06 20:14:34 +0300

    The Last

--- Program.cs
+++ Program.cs
@@ -3,28 +3,28 @@
 
 
 
-decimal firstAge ;
-decimal secondAge ; 
-Console.WriteLine("Son yozing ");
-Console.Write("Masalan ");
-string inputFirstAge = 
-Console.Write("Masalan2 ");
- string inputSecondAge = (Console.ReadLine());
- int usersAge = Convert.ToDecimal(usersAge);
+// decimal firstAge ;
+// decimal secondAge ; 
+// Console.WriteLine("Son yozing ");
+// Console.Write("Masalan ");
+// string inputFirstAge = 
+// Console.Write("Masalan2 ");
+//  string inputSecondAge = (Console.ReadLine());
+//  int usersAge = Convert.ToDecimal(usersAge);
 // Console.WriteLine("Natija= " );
 
 
-// int askarYoshi;
-// Console.WriteLine("Yoshingizni kiriting" );
-// Console.Write("Age: ");
-// askarYoshi = Convert.ToInt32(Console.ReadLine());
-// if (askarYoshi < 18)
-// {
-//     Console.WriteLine("Siz bora olmaysiz");
+int askarYoshi;
+Console.WriteLine("Yoshingizni kiriting" );
+Console.Write("Age: ");
+askarYoshi = Convert.ToInt32(Console.ReadLine());
+if (askarYoshi < 18)
+{
+    Console.WriteLine("Siz bora olmaysiz");
 
-// }
-// else
-// {
-//     if (askarYoshi >= 18)    
-//     Console.WriteLine("Siz bora olasiz");
-// }
+}
+else
+{
+    if (askarYoshi >= 18)    
+    Console.WriteLine("Siz bora olasiz");
+}
